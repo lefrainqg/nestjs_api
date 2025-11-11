bcfd7e799aebf2404709eae148390197c7054c3d
Efrain QG <lefrainqg@gmail.com>
2025-11-11 12:17:06 -0500

    feat: Shared refactorizacion de importaciones

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 
-import { HttpsCustomService } from '@shared/services/https.service';
-import { ExceptionManagerService } from '@shared/services/exception-manager.service';
+import { HttpsCustomService } from '@sharedServices/https.service';
+import { ExceptionManagerService } from '@sharedServices/exception-manager.service';
 
 @Module({
 
